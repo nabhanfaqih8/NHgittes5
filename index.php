2bf61fc9d85ded8018ab55173548ff6be43cbbed
nabhanfaqih8 <nabhanfaqih8@gmail.com>
2020-09-17 11:09:07 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 <body>
     <h1>Hello Word!</h1>
     <p1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto neque nobis maiores, labore vitae quibusdam rerum, cum at nostrum commodi sit excepturi dolores ut ab eligendi, in accusamus itaque suscipit.
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
+
+
 </body>
 </html>
